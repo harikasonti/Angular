1c91f2ed3a8eeb54d492842b8949ae17766d12e5
harikasonti <96762650+harikasonti@users.noreply.github.com>
2022-04-06 09:15:31 +0530

    Delete businesshouse.spec.ts

--- businesshouse.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Businesshouse } from './businesshouse';
-
-describe('Businesshouse', () => {
-  it('should create an instance', () => {
-    expect(new Businesshouse()).toBeTruthy();
-  });
-});
